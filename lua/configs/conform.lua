248f570d36756122e11d9840493fbe6854ba339c
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-09 07:55:49 +0900

    Back to Tab 4

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,66 +1,66 @@
 local options = {
-    formatters_by_ft = {
-        lua = { "stylua" },
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        ruby = { "rufo" },
-        eruby = { "erb_format" },
-        -- go = { "gofumpt", "goimports-reviser", "golines" },
-        -- haskell = { "fourmolu", "stylish-haskell" },
-        -- python = { "isort", "black" },
-    },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+    ruby = { "rufo" },
+    eruby = { "erb_format" },
+    -- go = { "gofumpt", "goimports-reviser", "golines" },
+    -- haskell = { "fourmolu", "stylish-haskell" },
+    -- python = { "isort", "black" },
+  },
 
-    formatters = {
-        -- -- C & C++
-        ["clang-format"] = {
-            prepend_args = {
-                "-style={ \
-                        IndentWidth: 2, \
-                        TabWidth: 2, \
+  formatters = {
+    -- -- C & C++
+    ["clang-format"] = {
+      prepend_args = {
+        "-style={ \
+                        IndentWidth: 4, \
+                        TabWidth: 4, \
                         UseTab: Never, \
                         AccessModifierOffset: 0, \
                         IndentAccessModifiers: true, \
                         PackConstructorInitializers: Never}",
-            },
-        },
-        -- -- Golang
-        -- ["goimports-reviser"] = {
-        --     prepend_args = { "-rm-unused" },
-        -- },
-        -- golines = {
-        --     prepend_args = { "--max-len=80" },
-        -- },
-        -- -- Lua
-        -- stylua = {
-        --     prepend_args = {
-        --         "--column-width", "80",
-        --         "--line-endings", "Unix",
-        --         "--indent-type", "Spaces",
-        --         "--indent-width", "4",
-        --         "--quote-style", "AutoPreferDouble",
-        --     },
-        -- },
-        -- -- Python
-        -- black = {
-        --     prepend_args = {
-        --         "--fast",
-        --         "--line-length",
-        --         "80",
-        --     },
-        -- },
-        -- isort = {
-        --     prepend_args = {
-        --         "--profile",
-        --         "black",
-        --     },
-        -- },
+      },
     },
+    -- -- Golang
+    -- ["goimports-reviser"] = {
+    --     prepend_args = { "-rm-unused" },
+    -- },
+    -- golines = {
+    --     prepend_args = { "--max-len=80" },
+    -- },
+    -- -- Lua
+    -- stylua = {
+    --     prepend_args = {
+    --         "--column-width", "80",
+    --         "--line-endings", "Unix",
+    --         "--indent-type", "Spaces",
+    --         "--indent-width", "4",
+    --         "--quote-style", "AutoPreferDouble",
+    --     },
+    -- },
+    -- -- Python
+    -- black = {
+    --     prepend_args = {
+    --         "--fast",
+    --         "--line-length",
+    --         "80",
+    --     },
+    -- },
+    -- isort = {
+    --     prepend_args = {
+    --         "--profile",
+    --         "black",
+    --     },
+    -- },
+  },
 
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 1500,
-        lsp_fallback = true,
-    },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 1500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
